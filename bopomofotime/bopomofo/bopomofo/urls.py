fa4d6ee8309708ed412e77464b017146eb7327a1
Serena Wang <serenawang621@gmail.com>
2021-09-01 22:56:15 -0700

    Add files via upload

--- bopomofotime/bopomofo/bopomofo/urls.py
+++ bopomofotime/bopomofo/bopomofo/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
+    path('', include('django.contrib.auth.urls')),
 ]
